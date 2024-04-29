518b0878deac3b7e87829a8aad0a017cdaee6dcd
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 11:07:22 +0600

    initial commit

---
+++ local-rest-json.js
@@ -0,0 +1,14 @@
+let rest ={
+    "host": "localhost",
+    "port": 3030,
+    "public": "../public/",
+    "paginate": {
+      "default": 10,
+      "max": 50
+    },
+    "mongodb": "mongodb://localhost:27017/api"
+}
+
+console.log(rest.public);
+console.log(rest.paginate.max);
+console.log(rest.mongodb);
